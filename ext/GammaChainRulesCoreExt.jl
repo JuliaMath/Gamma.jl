5d1f401fb7c40a42c2c565ca12d1eb4a0a7ec66e
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2026-07-02 19:21:57 -0400

    add Mooncake and Enzyme rules

--- ext/GammaChainRulesCoreExt.jl
+++ ext/GammaChainRulesCoreExt.jl
@@ -1,8 +1,9 @@
 module GammaChainRulesCoreExt
-    using Gamma
-    using ChainRulesCore
 
-    ChainRulesCore.@scalar_rule Gamma.gamma(x)      Gamma.digamma(x) * Gamma.gamma(x)
-    ChainRulesCore.@scalar_rule Gamma.loggamma(x)   Gamma.digamma(x)
+using Gamma
+using ChainRulesCore
+
+ChainRulesCore.@scalar_rule Gamma.gamma(x)      Gamma.digamma(x) * Gamma.gamma(x)
+ChainRulesCore.@scalar_rule Gamma.loggamma(x)   Gamma.digamma(x)
 
 end #module
